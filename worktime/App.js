d937fc42a27d3571383f589a70b9767e0c8bd8e8
and96437345 <and96437345@yandex.ru>
2024-04-10 23:22:16 +0300

    Worked version

--- worktime/App.js
+++ worktime/App.js
@@ -1,25 +1,23 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { Button, Text } from 'react-native';
-import HomeScreen from './components/Homescreen';
-import ProfileScreen from './components/ProfileScreen';
+import EnterPage from './components/EnterPage';
+import RegistrationPage from './components/RegistrationPage';
 
 const App = () => {
 
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
-const MyStack = () => {
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Enter" component={HomeScreen}/>
-        <Stack.Screen name="Registration" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+  const MyStack = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Enter" component={EnterPage}/>
+          <Stack.Screen name="Registration" component={RegistrationPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
 
   return (
     <MyStack/>
